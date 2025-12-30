32e25475a4b5ecc7ce584f7b2653d0066738dabe
galih342 <126547880+galih342@users.noreply.github.com>
2025-12-30 08:15:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
     
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     app.register_blueprint(admin_bp, url_prefix="/admin")
     app.register_blueprint(auth_bp)
