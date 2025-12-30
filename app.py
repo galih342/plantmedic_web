908cc378cc522032060f6c0ede55c8a250076950
galih342 <126547880+galih342@users.noreply.github.com>
2025-12-30 08:03:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,13 +32,18 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = "plantmedic-secret-key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = ("mysql+pymysql://root:@localhost/plantmedic_db")
+    # KUNCI UTAMA:
+    # Mengambil database dari Railway, kalau gak ada (di laptop) pake localhost.
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost/plantmedic_db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["JWT_SECRET_KEY"] = "super-secret-key"
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret-key")
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
     db.init_app(app)
     jwt.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(admin_bp, url_prefix="/admin")
     app.register_blueprint(auth_bp)
